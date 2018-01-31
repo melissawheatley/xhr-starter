e07b04bca0a34cf04ec3dd7d766403fcdd489431
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-01-31 11:04:58 -0600

    finished class codealong

--- js/main.js
+++ js/main.js
@@ -11,4 +11,56 @@ for (let i = 0; i < 2000000; i++){
 
 console.log("newTime: ", Date.now());
 
+let bigDataRequest = new XMLHttpRequest();
 
+bigDataRequest.addEventListener("load", bigDataComplete);
+bigDataRequest.addEventListener("error", bigDataFailed);
+
+function bigDataComplete(event){
+    console.log("event: ", event);
+    if(event.target.status === 200){
+        let bigData = JSON.parse(event.target.responseText);
+        console.log("Time of Big Data: ", Date.now() - startTime);
+        console.log("bigData", bigData);
+    }else{
+        console.log("check the spelling of your file");
+    }
+}
+
+function bigDataFailed(event){
+    console.log("something went wrong");
+}
+
+bigDataRequest.open("GET", "JEOPARDY_QUESTIONS1.json");
+bigDataRequest.send();
+
+let dataRequest = new XMLHttpRequest();
+
+dataRequest.addEventListener("load", dataRequestComplete);
+dataRequest.addEventListener("error", dataRequestFailed);
+
+function dataRequestComplete(event) {
+    console.log("colors loaded successfully");
+    let colorData = JSON.parse(event.target.responseText);
+    console.log("colorData: ", colorData);
+    showData(colorData);
+}
+
+function showData(taco){
+    let colorDiv = document.getElementById("all-my-colors");
+    let colorData = '';
+    for(let item in taco){
+        let colorItem = taco[item];
+        colorData += `<div><h2>${colorItem.color}: ${colorItem.value}</h2></div>`
+    };
+
+    colorDiv.innerHTML=colorData;
+    console.log("the colors are done ", Date.now() - startTime);
+}
+
+function dataRequestFailed(event){
+    console.log("colors failed to load", event);
+}
+
+dataRequest.open("GET", "color.json");
+dataRequest.send();
